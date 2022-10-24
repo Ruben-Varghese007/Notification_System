3954989fb380c2e46dd52d13e58521e9c82e4bf6
Ruben-Varghese007 <116579510+Ruben-Varghese007@users.noreply.github.com>
2022-10-24 20:58:52 +0530

    Notification-Alert
    Develop a python program that send notification in window as well as the mobile.

---
+++ Notify.py
@@ -0,0 +1,39 @@
+from winotify import Notification,audio
+import smtplib
+from email.message import EmailMessage
+
+def notifyMe(name,message):
+    toast = Notification(app_id="Windows App",
+                         title="New Notification!",
+                         msg=message,
+                         icon="D:\Internship\TechSnap\Logo\icon.png")
+    toast.set_audio(audio.Default,loop = False)
+    toast.show()
+
+def email_alert(subject,body,to):
+
+    text=EmailMessage()
+    text.set_content(body)
+    text['subject'] = subject
+    text['to'] = to
+
+    user = "emailnotifyalert01@gmail.com"
+    text['from'] = user
+    password = "jaltgizkhiopendv"
+
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(user,password)
+    server.send_message(text)
+    server.quit()
+
+def main():
+
+    name = input("Enter your name : ")
+    email = input("Enter your mail id : ")
+    message = "Hey "+name+" Take a Break !\nAn Email has been sent to : \n"+email
+    notifyMe(name,message)
+    email_alert("Heavy Usage Warning !","You have been using this device for more than 3 hours.\nPlease Take a Break !",email)
+    print("Email sent Successfully")
+
+main()
